d439322ad3f084924a71e408150744f91f0c21fd
Tagaev Manas <manastagaev@Manas-MacBook-Air.local>
2024-05-23 19:46:10 +0900

    working draft added

---
+++ constants.py
@@ -0,0 +1,3 @@
+# Replace with your own OpenAI API Key https://platform.openai.com/account/api-keys
+# and rename this file to constants.py.
+APIKEY = "sk-proj-lqo5Gt3cRCCldkkKNC6CT3BlbkFJdF7MhDkDEbfY4CGtRzpY"
